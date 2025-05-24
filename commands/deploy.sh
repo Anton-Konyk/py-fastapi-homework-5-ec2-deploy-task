7b01466acd9b9aee1a8633d6e64546afebd88905
Anton Konyk <antonkonyk.com>
2025-05-24 18:26:42 +0200

    add docker stopping and removing existing containers

--- commands/deploy.sh
+++ commands/deploy.sh
@@ -24,6 +24,10 @@ git reset --hard origin/main || handle_error "Failed to reset the local reposito
 echo "Fetching tags from the remote repository..."
 git fetch origin --tags || handle_error "Failed to fetch tags from the 'origin' remote."
 
+# Stop and remove existing containers
+echo "🧹 Stopping and removing existing containers..."
+docker compose -f docker-compose-prod.yml down --remove-orphans -v || handle_error "Failed to stop and remove old containers."
+
 # Build and run Docker containers with Docker Compose v2
 docker compose -f docker-compose-prod.yml up -d --build || handle_error "Failed to build and run Docker containers using docker-compose-prod.yml."
 
